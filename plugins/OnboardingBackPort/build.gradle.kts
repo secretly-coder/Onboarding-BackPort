e8f3e7383c1d977f6e62eec392bbac2a027d3f6d
Ribro <deepak76dch@gmail.com>
2026-06-15 09:47:06 +0530

    Removed unused extension block to fix build error

--- plugins/OnboardingBackPort/build.gradle.kts
+++ plugins/OnboardingBackPort/build.gradle.kts
@@ -1,5 +1,1 @@
-import com.aliucord.gradle.AliucordExtension
-
-configure<AliucordExtension> {
-    description.set("Backports Discord's server onboarding feature to Aliucord.")
-}
+// This file is intentionally left blank. Master config handles everything.
